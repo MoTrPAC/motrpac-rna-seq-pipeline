9a9c3af32c3d7a32d35e5594aa56db8dcb986d19
Archana Raja <araja7@stanford.edu>
2019-04-30 15:26:57 -0700

    Cleaned up the script

--- scripts/make_json_rnaseq.py
+++ scripts/make_json_rnaseq.py
@@ -36,18 +36,3 @@ for i in filelist:
   simplejson.dump(d, f)
   f.close()
 
-#>>> r1="gs://motrpac-portal-transfer-sinai/rna-seq/PASS1A/80000885506_R1.fastq.gz"
-#>>> r2=r1.replace("_R1.fastq.gz","_R2.fastq.gz")
-#>>> r2
-#'gs://motrpac-portal-transfer-sinai/rna-seq/PASS1A/80000885506_R2.fastq.gz'
-#>>> i1=r1.replace("_R1.fastq.gz","_I1.fastq.gz")
-#>>> i1
-#'gs://motrpac-portal-transfer-sinai/rna-seq/PASS1A/80000885506_I1.fastq.gz'
-#>>> a='gs://motrpac-portal-transfer-sinai/rna-seq/PASS1A/90028013001_I1.fastq.gz'
-#>>> a.split("/")
-#['gs:', '', 'motrpac-portal-transfer-sinai', 'rna-seq', 'PASS1A', '90028013001_I1.fastq.gz']
-#>>> a.split("/")[-1]
-#'90028013001_I1.fastq.gz'
-#>>> a.split("/")[-1].split("_I1.fastq.gz")[0]
-#'90028013001'
-#gs://rna-seq_araja/references/rn/v96/star_2.7.0d_04-20-19/sorted/Rnor6_v96_star_index.tar.gz
