8bc9ed99981d914db55a01535796b7d0d7ec2f66
Nicole Gay <nicolerg@stanford.edu>
2020-09-30 15:39:11 -0700

    Create bam2bigwig.sh

---
+++ scripts/bam2bigwig.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# convert a single BAM to bigwig format
+# usage: bash bam2bigwig.sh /path/to/90251015803.Aligned.sortedByCoord.out /path/to/bigwigs
+# this command will generate /path/to/bigwigs/90251015803.bw 
+#
+# dependencies: samtools, deeptools
+
+bam=$1
+outdir=$2
+
+prefix=$(basename ${bam} | sed "s/\.Aligned.*//")
+samtools index ${bam}
+bamCoverage -b ${bam} -o ${outdir}/${prefix}.bw
