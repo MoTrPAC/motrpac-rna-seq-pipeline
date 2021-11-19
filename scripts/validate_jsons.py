e22b55d31f2783fc443ce2cfb26fb967c9c00ad3
mihirsamdarshi <msamdars@stanford.edu>
2021-11-18 19:09:26 -0800

    fix: fix scripts to be better

--- scripts/validate_jsons.py
+++ scripts/validate_jsons.py
@@ -1,21 +1,32 @@
-#Usage : python3 scripts/validate_jsons.py input_json/test3/set3_rnaseq.json input_json/test4/test_batchac_rnaseq.json
+"""
+Usage:
+python3 scripts/validate_jsons.py input_json/test3/set3_rnaseq.json
+            input_json/test4/test_batchac_rnaseq.json
+"""
 import argparse
 import json
 
-parser = argparse.ArgumentParser(description='script to compare and validate json files')
-parser.add_argument('infile1', type=str, 
-                    help='Input json filename')
-parser.add_argument('infile2', type=str , help ='Name of the second input json')
-args = parser.parse_args()
-#json_file1=open("/Users/archanaraja/work/repo/motrpac-rna-seq-pipeline/input_json/test_batchsubset_rnaseq.json")
-#json_file2=open("/Users/archanaraja/work/repo/motrpac-rna-seq-pipeline/input_json/test2/set1_rnaseq.json")
-json_file1=open(args.infile1)
-json_file2=open(args.infile2)
-a=json.load(json_file1)
-b=json.load(json_file2)
-a, b = json.dumps(a, sort_keys=True), json.dumps(b, sort_keys=True)
-print ("Validation results")
-if (a == b) :
-    print ("Two jsons are identical")
-elif (a != b) :
-   print ("Two jsons don't match") 
+
+def main():
+    parser = argparse.ArgumentParser(description="script to compare and validate json files")
+    parser.add_argument("infile1", type=str, help="Input json filename")
+    parser.add_argument("infile2", type=str, help="Name of the second input json")
+    args = parser.parse_args()
+
+    with open(args.infile1, encoding="utf-8") as json_file1:
+        file_a = json.load(json_file1)
+
+    with open(args.infile1, encoding="utf-8") as json_file2:
+        file_b = json.load(json_file2)
+
+    a, b = json.dumps(file_a, sort_keys=True), json.dumps(file_b, sort_keys=True)
+
+    print("Validation results")
+    if a == b:
+        print("Two jsons are identical")
+    elif a != b:
+        print("Two jsons don't match")
+
+
+if __name__ == "__main__":
+    main()
--- wdl/checksum/make_json_checksum.py
+++ wdl/checksum/make_json_checksum.py
@@ -1,21 +1,33 @@
-# This script generated input json files for a set of files for computing md5sums
+"""
+This script generated input json files for a set of files for computing md5sums
+
+Usage : python make_json.py comma_separated_list_of_files path_to_run_folder
+"""
 import sys
 import os
 import simplejson
-#Usage : python make_json.py comma_separated_list_of_files path_to_run_folder
-#python make_json.py segaa,segab,segac,segad,segae,segaf,segag,segah /Users/archanaraja/work/repo/motrpac-rna-seq-pipeline/checksum/sinai/rrbs/
-#filelist=["sinaib1aa","sinaib1ab","sinaib1ac","sinaib1ad","sinaib1ae","sinaib1af","sinaib1ag","sinaib1ah"]
-#filelist=["stanford_b1rnaaa","stanford_b1rnaab","stanford_b1rnaac","stanford_b1rnaad","stanford_b1rnaae","stanford_b1rnaaf","stanford_b1rnaag","stanford_b1rnaah","stanford_b1rnaai"]
-filelist=sys.argv[1].split(',')
-output_path=sys.argv[2]
-os.chdir(output_path)
-for i in filelist:
-#  fn=i+".json"
-#  file_path=os.path.join(output_path,fn)
-  f=open(i+".json","w")
-#  f=open(file_path,"w")
-  my_list = [line.strip("\n") for line in open(i)]
-  d= {"checksum_workflow.checksum.num_preempt": "0","checksum_workflow.checksum.memory": "20","checksum_workflow.checksum.disk_space": "30","checksum_workflow.checksum.num_threads": "4","checksum_workflow.checksum.docker": "gcr.io/***REMOVED***/motrpac_rnaseq:v0.1_04_20_19","checksum_workflow.sample_files":my_list}
-  simplejson.dump(d, f)
-  f.close()
 
+
+def main():
+    filelist = sys.argv[1].split(",")
+    output_path = sys.argv[2]
+    os.chdir(output_path)
+    for filename in filelist:
+        with open(f"{filename}.json", "w", encoding="utf-8") as file:
+            my_list = [line.strip("\n") for line in file]
+
+        json_d = {
+            "checksum_workflow.checksum.num_preempt": "0",
+            "checksum_workflow.checksum.memory": "20",
+            "checksum_workflow.checksum.disk_space": "30",
+            "checksum_workflow.checksum.num_threads": "4",
+            "checksum_workflow.checksum.docker": "gcr.io/***REMOVED***/motrpac_rnaseq:v0.1_04_20_19",
+            "checksum_workflow.sample_files": my_list,
+        }
+
+        with open(f"{filename}.json", "w", encoding="utf-8") as outfile:
+            simplejson.dump(json_d, outfile)
+
+
+if __name__ == "__main__":
+    main()
