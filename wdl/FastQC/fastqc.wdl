77c304286b86ab4eaf0dafbd9694d68f928a650d
mihirsamdarshi <msamdars@stanford.edu>
2021-11-18 18:02:07 -0800

    refactor: reorganize repository, rename directories

---
+++ wdl/FastQC/fastqc.wdl
@@ -0,0 +1,53 @@
+task fastQC {
+    File fastqr1
+    File fastqr2
+    String outdir
+    Int memory
+    Int disk_space
+    Int num_threads
+    Int num_preempt
+    String docker
+
+    command {
+        mkdir -p ${outdir}
+        fastqc -o ${outdir} ${fastqr1}
+        fastqc -o ${outdir} ${fastqr2}
+
+        tar -cvzf ${outdir}.tar.gz ./${outdir}
+    }
+    output {
+        File fastQC_report = '${outdir}.tar.gz'
+    }
+    runtime {
+        docker: "${docker}"
+        memory: "${memory}GB"
+        disks: "local-disk ${disk_space} HDD"
+        cpu: "${num_threads}"
+        preemptible: "${num_preempt}"
+    }
+}
+
+workflow fastqc_report {
+    File fastqr1
+    File fastqr2
+    Int memory
+    String outdir
+    Int disk_space
+    Int num_threads
+    Int num_preempt
+    String docker
+    call fastQC {
+        input:
+            fastqr1 = fastqr1,
+            fastqr2 = fastqr2,
+            outdir=outdir,
+            memory=memory,
+            disk_space=disk_space,
+            num_threads=num_threads,
+            num_preempt=num_preempt,
+            docker=docker
+    }
+    output {
+        fastQC.fastQC_report
+    }
+}
---
+++ wdl/mark_duplicates/run_MarkDuplicates.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import struct
+import subprocess
+from datetime import datetime
+import contextlib
+
+
+@contextlib.contextmanager
+def cd(cd_path):
+    saved_path = os.getcwd()
+    os.chdir(cd_path)
+    yield
+    os.chdir(saved_path)
+
+
+parser = argparse.ArgumentParser(
+    description='Convert BAM to FASTQ using SamToFastq from Picard.'
+)
+parser.add_argument('input_bam', type=str, help='BAM file')
+parser.add_argument(
+    'prefix', type=str, help='Prefix for output files; usually <sample_id>'
+)
+parser.add_argument('-o', '--output_dir', default=os.getcwd(), help='Output directory')
+parser.add_argument('-m', '--memory', default='3', type=str, help='Memory, in GB')
+parser.add_argument(
+    '--optical_duplicate_pixel_distance', default=100,
+    help='Maximum offset between two duplicate clusters. 100 (default) is appropriate '
+         'for unpatterned, 2500 recommended for patterned flowcells.'
+)
+parser.add_argument(
+    '--jar', default='/opt/picard-tools/picard.jar', help='Path to Picard jar'
+)
+args = parser.parse_args()
+
+print(
+    '[' + datetime.now().strftime("%b %d %H:%M:%S") + '] Starting MarkDuplicates',
+    flush=True
+)
+
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
+
+with cd(args.output_dir):
+    subprocess.check_call(
+        'java -jar -Xmx' + args.memory + 'g ' + args.jar \
+        + ' MarkDuplicates I=' + args.input_bam \
+        + ' O=' + os.path.split(args.input_bam)[1].replace('.bam', '.md.bam') \
+        + ' M=' + args.prefix + '.marked_dup_metrics.txt' + ' ASSUME_SORT_ORDER=coordinate OPTICAL_DUPLICATE_PIXEL_DISTANCE=' + str(
+            args.optical_duplicate_pixel_distance
+            ), shell=True
+        ) \
+    + ' CREATE_INDEX=true' \
+    + ' REMOVE_DUPLICATES=false' \
+    + ' VALIDATION_STRINGENCY=SILENT'
+
+print(
+    '[' + datetime.now().strftime("%b %d %H:%M:%S") + '] Finished MarkDuplicates',
+    flush=True
+)
