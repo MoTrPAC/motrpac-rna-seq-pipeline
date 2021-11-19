484ceb5ab3692a741e981cd5a54b74797e712d49
mihirsamdarshi <msamdars@stanford.edu>
2021-11-18 19:09:26 -0800

    fix: fix scripts to be better

--- wdl/dup_umi/UMI_dup.sh
+++ wdl/dup_umi/UMI_dup.sh
@@ -2,16 +2,16 @@
 set -euo pipefail
 #working the nudup to get the log file for the PCR duplication rates
 #Default is 8 bases, the script obtains it from the readname
-bam=$(readlink -e $1)
+bam=$(readlink -e "$1")
 paired=$2
 pairopt=""
-if (( $paired == 1 )); then
+if (( paired == 1 )); then
     pairopt="-2"
 fi
 
-SID=$(basename $(dirname $bam))
+SID=$(basename "$(dirname "$bam")")
 set +e #head has a problem with this
-len=$(samtools view $bam |head -1 |awk '{umi=gensub("^.*:","",1,$1); print length(umi)}')
+len=$(samtools view "$bam" |head -1 |awk '{umi=gensub("^.*:","",1,$1); print length(umi)}')
 set -e
 
 cd star_align/$SID
